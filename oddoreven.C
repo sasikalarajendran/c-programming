db30dced758292c15241c1346d66422b1f4c333e
sasikalarajendran <sasikalarvsb@gmail.com>
2017-06-19 16:28:42 +0530

    odd or even
    c programming

---
+++ oddoreven.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter a number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("\n The number is even");
+}
+else
+{
+printf("The number is odd");
+}
+return 0;
+}

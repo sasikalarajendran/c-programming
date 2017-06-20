6d0cca2ebad0d3a60cabb8101525a677d640877b
sasikalarajendran <sasikalarvsb@gmail.com>
2017-06-20 12:49:22 +0530

    power of a number
    c programme

---
+++ power.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int base,exp,res;
+printf("enter the base and exp");
+scanf("%d%d",&base,&exp);
+res=res*base;
+exp--;
+printf("the power of a number");
+return 0;
+}
+
+
